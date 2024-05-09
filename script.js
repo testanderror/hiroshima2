7dd88807185037ad45244e1fb38e08d2151e2198
testanderror <108211073+testanderror@users.noreply.github.com>
2024-05-09 10:39:55 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ menuBtn.addEventListener('click', () => {
 closeMenuBtn.addEventListener('click', () => {
   menuBtn.click();
 });
+
+var swiper = new Swiper('.slide-content', {
+  slidesPerView: 1,
+  spaceBetween: 40,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
